721ab0bd995ddaceae3e0d61a96642bcb206e2ff
xjeyceex <jcmiguel.beltran@gmail.com>
2025-04-08 10:13:57 +0800

    remove console error

--- app/(protected)/users/page.tsx
+++ app/(protected)/users/page.tsx
@@ -300,6 +300,7 @@ const UsersPage = () => {
                 fullWidth
                 variant="outline"
                 mt="sm"
+                scroll={false}
               >
                 View Profile
               </Button>
